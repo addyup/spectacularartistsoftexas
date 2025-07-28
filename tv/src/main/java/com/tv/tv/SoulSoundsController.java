dd3be80e3c7eba72481200dbefdc9bd766d3908c
adduup <adwait.uprety21@gmail.com>
2025-07-27 19:32:06 -0500

    pls

--- tv/src/main/java/com/tv/tv/SoulSoundsController.java
+++ tv/src/main/java/com/tv/tv/SoulSoundsController.java
@@ -17,7 +17,7 @@ public class SoulSoundsController {
 private static Document doc;
 
      @GetMapping("/alcohol")
-     @CrossOrigin(origins = "https://spectacularartistsoftexas.netlify.app/artists.html")
+     @CrossOrigin(origins = "https://spectacularartistsoftexas.netlify.app")
   public String[] soundStacksHealth() throws Exception{
      doc = Jsoup.connect("https://www.betterhealth.vic.gov.au/health/healthyliving/alcohol").get();
      Element targetDiv = doc.selectFirst(".rpl-markup__inner");
