d7f6ba1d0b7c5ae3ae5da79e16f9ca4ad2d9929e
adduup <adwait.uprety21@gmail.com>
2025-08-04 08:14:35 -0500

    so far..

---
+++ tv/src/main/java/com/tv/tv/NewsScraper.java
@@ -0,0 +1,53 @@
+package com.tv.tv;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/news")
+@CrossOrigin(origins = "*")
+public class NewsScraper {
+
+    @Autowired
+private static Document doc;
+
+     @GetMapping("/tc")
+     @CrossOrigin(origins = "*")
+  public List<String> soundStacksHealth() throws Exception{
+     doc = Jsoup.connect("https://techcrunch.com/").get();
+     
+        Element div = doc.selectFirst("div.wp-block-columns");
+
+            
+
+
+ List<String> elementStrings = new ArrayList<>();
+
+        if (div != null) {
+            Elements children = div.children();  // Only direct children. Use div.select("*") for all descendants
+
+            for (Element child : children) {
+                elementStrings.add(child.outerHtml());
+            }
+        }
+
+        // Print results
+        for (String element : elementStrings) {
+            System.out.println("Element: " + element);
+        }
+    
+    return elementStrings;
+      }
+      
+    
+}
